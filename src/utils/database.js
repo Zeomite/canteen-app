c707619d32a6180b234d7bbbb5668af2a4e5ac96
ZeoMite <105901968+Zeomite@users.noreply.github.com>
2023-09-10 11:22:44 +0530

    Update database.js

--- src/utils/database.js
+++ src/utils/database.js
@@ -8,7 +8,7 @@ const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   port: DB_PORT,
-  dialect: 'postgres', // You can adjust this based on your database
+  dialect: 'postgres',
 });
 
 
